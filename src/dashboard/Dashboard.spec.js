7f5a4e9127c61bc4f2779637b85beb3508e77568
rae-glazier <glazier.home.life@gmail.com>
2019-12-18 17:42:22 -0600

    update

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,7 +1,12 @@
 // Test away
+import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
 // what i want the testing to do 
 // make sure the display is showing / working
 // make sure the controls are there / working
+
+test('dashboard renders w/o crashing', () => {
+    render(<Dashboard />)
+})
