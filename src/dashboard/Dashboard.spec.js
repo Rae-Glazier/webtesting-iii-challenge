288a571fdfabd11339ea45c9d2907692728ec7cc
rae-glazier <glazier.home.life@gmail.com>
2019-12-17 22:08:36 -0600

    stuffs

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,7 @@
 // Test away
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+// what i want the testing to do 
+// make sure the display is showing / working
+// make sure the controls are there / working
